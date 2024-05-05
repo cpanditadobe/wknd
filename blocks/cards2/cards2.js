7b943a51cb3d3ebcd65cfaf784edb3a194069c5b
Chinmay Pandit <cpandit@adobe.com>
2024-05-05 22:22:52 +0530

    commit cards2

--- blocks/cards2/cards2.js
+++ blocks/cards2/cards2.js
@@ -1,5 +1,5 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
-
+//submitting again
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
